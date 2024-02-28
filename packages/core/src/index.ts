8e89a29650139977807e2ec0e6ef997227e1c099
Matteo Bilotta <me@byloth.dev>
2024-02-28 02:30:26 +0100

    add: Added the possibility to add custom properties to the emitted alert.

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -11,7 +11,7 @@ export type { IAction, ActionCallback, ActionOptions } from "./types/action/inde
 export type { CallbackAction } from "./types/action/callback.js";
 export type { ValueAction } from "./types/action/value.js";
 
-export type { IAlert, AlertOptions } from "./types/alert/index.js";
+export type { IAlert, AlertOptions, AlertCustomOptions } from "./types/alert/index.js";
 export type { SimpleAlert, BlockingAlert, DismissibleAlert } from "./types/alert/simple.js";
 export type { CustomAlert, BlockingCustomAlert, DismissibleCustomAlert } from "./types/alert/custom.js";
 export type { PluginOptions } from "./functions.js";
--- packages/core/src/types/alert/index.ts
+++ packages/core/src/types/alert/index.ts
@@ -3,4 +3,6 @@ import type { CustomAlert } from "./custom.js";
 
 export type { IAlert } from "./core.js";
 
-export type AlertOptions<R = void, P extends Record<string, unknown> = never> = SimpleAlert<R, P> | CustomAlert<R, P>;
+export interface AlertCustomOptions { }
+export type AlertOptions<R = void, P extends Record<string, unknown> = never> =
+    (SimpleAlert<R, P> | CustomAlert<R, P>) & AlertCustomOptions;
